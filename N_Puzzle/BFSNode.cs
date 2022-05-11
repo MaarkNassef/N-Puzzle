4cbe90ac82178cc2d19f8d523549cc32331aa818
Omargamalcs <omar2001gamal2001@gmail.com>
2022-05-11 20:45:37 +0200

    Update

---
+++ N_Puzzle/BFSNode.cs
@@ -0,0 +1,185 @@
+﻿namespace N_Puzzle
+{
+    public class BFSNode
+    {
+        public BFSNode? Parent;
+        public int[,] Matrix;
+        public int G;
+        public int H;
+        public Type type;
+        public Tuple<int, int> ZeroPlace;
+        public char? PreferredStep;
+        public BFSNode(int[,] matrix, BFSNode? parent = null, int G = 0, char PreferredStep = '\0', Type type = Type.HAMMING)
+        {
+            this.Matrix = matrix;
+            this.type = type;
+            this.Parent = parent;
+            this.ZeroPlace = new Tuple<int, int>(matrix.GetLength(0), matrix.GetLength(1)); //Garbage.
+            this.PreferredStep = PreferredStep;
+            this.G = G;
+            if (type.Equals(Type.HAMMING))
+            {
+                this.H = findHamming();
+            }
+            else if (type.Equals(Type.MANHATTEN))
+            {
+                this.H = findManhatten();
+            }
+        }
+        private int findManhatten()
+        {
+            H = 0;
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    if (Matrix[i, j] == 0)
+                    {
+                        this.ZeroPlace = new Tuple<int, int>(i, j);
+                        continue;
+                    }
+                    Tuple<int, int> placeInGoal = findPlaceInGoal(Matrix[i, j]);
+                    H += Math.Abs(i - placeInGoal.Item1) + Math.Abs(j - placeInGoal.Item2);
+                }
+            }
+            return H;
+        }
+        private int findHamming()
+        {
+            H = 0;
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    if (Matrix[i, j] == 0)
+                    {
+                        this.ZeroPlace = new Tuple<int, int>(i, j);
+                        continue;
+                    }
+                    Tuple<int, int> placeInGoal = findPlaceInGoal(Matrix[i, j]);
+                    if (!placeInGoal.Equals(new Tuple<int, int>(i, j)))
+                    {
+                        H++;
+                    }
+                }
+            }
+            return H;
+        }
+        private Tuple<int, int> findPlaceInGoal(int num)
+        {
+            if (num == 0)
+            {
+                return new Tuple<int, int>(Matrix.GetLength(0) - 1, Matrix.GetLength(1) - 1);
+            }
+            else
+            {
+                return new Tuple<int, int>((num - 1) / Matrix.GetLength(1), (num - 1) % Matrix.GetLength(1));
+            }
+        }
+        public Queue<BFSNode> AllPossibleMoves()
+        {
+            Queue<BFSNode> result = new Queue<BFSNode>();
+            //Move Up
+            BFSNode? move = MoveUp();
+            if (move != null)
+            {
+                result.Enqueue(move);
+            }
+            //Move Down
+            move = MoveDown();
+            if (move != null)
+            {
+                result.Enqueue(move);
+            }
+            //Move Left
+            move = MoveLeft();
+            if (move != null)
+            {
+                result.Enqueue(move);
+            }
+            //Move Right
+            move = MoveRight();
+            if (move != null)
+            {
+                result.Enqueue(move);
+            }
+            return result;
+        }
+        public BFSNode? MoveUp()
+        {
+            Tuple<int, int> newPoint = new Tuple<int, int>(ZeroPlace.Item1 - 1, ZeroPlace.Item2);
+            if (newPoint.Item1 >= 0 && (Parent == null || (Parent != null && !Parent.ZeroPlace.Equals(newPoint))))
+            {
+                int[,] change = new int[Matrix.GetLength(0), Matrix.GetLength(1)];
+                for (int i = 0; i < Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    {
+                        change[i, j] = Matrix[i, j];
+                    }
+                }
+                change[ZeroPlace.Item1, ZeroPlace.Item2] = change[newPoint.Item1, newPoint.Item2];
+                change[newPoint.Item1, newPoint.Item2] = 0;
+                return new BFSNode(change, this, G + 1, 'U', this.type);
+            }
+            return null;
+        }
+        public BFSNode? MoveDown()
+        {
+            Tuple<int, int> newPoint = new Tuple<int, int>(ZeroPlace.Item1 + 1, ZeroPlace.Item2);
+            if (newPoint.Item1 < Matrix.GetLength(0) && (Parent == null || (Parent != null && !Parent.ZeroPlace.Equals(newPoint))))
+            {
+                int[,] change = new int[Matrix.GetLength(0), Matrix.GetLength(1)];
+                for (int i = 0; i < Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    {
+                        change[i, j] = Matrix[i, j];
+                    }
+                }
+                change[ZeroPlace.Item1, ZeroPlace.Item2] = change[newPoint.Item1, newPoint.Item2];
+                change[newPoint.Item1, newPoint.Item2] = 0;
+                return new BFSNode(change, this, G + 1, 'D', this.type);
+            }
+            return null;
+        }
+        public BFSNode? MoveLeft()
+        {
+            Tuple<int, int> newPoint = new Tuple<int, int>(ZeroPlace.Item1, ZeroPlace.Item2 - 1);
+            if (newPoint.Item2 >= 0 && (Parent == null || (Parent != null && !Parent.ZeroPlace.Equals(newPoint))))
+            {
+                int[,] change = new int[Matrix.GetLength(0), Matrix.GetLength(1)];
+                for (int i = 0; i < Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    {
+                        change[i, j] = Matrix[i, j];
+                    }
+                }
+                change[ZeroPlace.Item1, ZeroPlace.Item2] = change[newPoint.Item1, newPoint.Item2];
+                change[newPoint.Item1, newPoint.Item2] = 0;
+                return new BFSNode(change, this, G + 1, 'L', this.type);
+            }
+            return null;
+        }
+        public BFSNode? MoveRight()
+        {
+            Tuple<int, int> newPoint = new Tuple<int, int>(ZeroPlace.Item1, ZeroPlace.Item2 + 1);
+            if (newPoint.Item2 < Matrix.GetLength(1) && (Parent == null || (Parent != null && !Parent.ZeroPlace.Equals(newPoint))))
+            {
+                int[,] change = new int[Matrix.GetLength(0), Matrix.GetLength(1)];
+                for (int i = 0; i < Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    {
+                        change[i, j] = Matrix[i, j];
+                    }
+                }
+                change[ZeroPlace.Item1, ZeroPlace.Item2] = change[newPoint.Item1, newPoint.Item2];
+                change[newPoint.Item1, newPoint.Item2] = 0;
+                return new BFSNode(change, this, G + 1, 'R', this.type);
+            }
+            return null;
+        }
+    }
+}
---
+++ N_Puzzle/BFSPuzzle.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Puzzle
+{
+    public class BFSPuzzle
+    {
+        public BFSNode Initial;
+        public bool isSolvable;
+        public int numOfSteps;
+        public Queue<BFSNode>? queue;
+        public BFSPuzzle(BFSNode Initial)
+        {
+            this.Initial = Initial;
+            this.isSolvable = IsSolvable();
+        }
+        private int Inversions()
+        {
+            int iterator = 0;
+            for (int i = 0; i < Initial.Matrix.GetLength(0) * Initial.Matrix.GetLength(1) - 1; i++)
+                for (int j = i + 1; j < Initial.Matrix.GetLength(0) * Initial.Matrix.GetLength(1); j++)
+                    if (Initial.Matrix[i / Initial.Matrix.GetLength(0), i % Initial.Matrix.GetLength(0)] > 0
+                        && Initial.Matrix[j / Initial.Matrix.GetLength(0), j % Initial.Matrix.GetLength(0)] > 0
+                        && Initial.Matrix[i / Initial.Matrix.GetLength(0), i % Initial.Matrix.GetLength(0)] > Initial.Matrix[j / Initial.Matrix.GetLength(0), j % Initial.Matrix.GetLength(0)])
+                        iterator++;
+            return iterator;
+        }
+        public bool IsSolvable()
+        {
+            int invCount = Inversions();
+            if (Initial.Matrix.GetLength(0) % 2 == 1 && invCount % 2 == 0)
+            {
+                return true;
+            }
+            else if (Initial.Matrix.GetLength(0) % 2 == 0 && Initial.ZeroPlace.Item1 % 2 == 0 && invCount % 2 == 1)
+            {
+                return true;
+            }
+            else if (Initial.Matrix.GetLength(0) % 2 == 0 && Initial.ZeroPlace.Item1 % 2 == 1 && invCount % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Stack<BFSNode> Solve()
+        {
+            Stack<BFSNode> stack = new Stack<BFSNode>();
+            if (isSolvable)
+            {
+                queue = Initial.AllPossibleMoves();
+                while (queue.Count > 0)
+                {
+                    BFSNode node = queue.Dequeue();
+                    if (node.H == 0)
+                    {
+                        this.numOfSteps = node.G;
+                        stack = GetRoot(node);
+                        break;
+                    }
+                    Queue<BFSNode> subQueue = node.AllPossibleMoves();
+                    while (subQueue.Count > 0)
+                    {
+                        BFSNode n = subQueue.Dequeue();
+                        queue.Enqueue(n);
+                    }
+                }
+            }
+            return stack;
+        }
+        public Stack<BFSNode> GetRoot(BFSNode node)
+        {
+            Stack<BFSNode> stack = new Stack<BFSNode>();
+            GetRootHelper(stack, node);
+            return stack;
+        }
+        private void GetRootHelper(Stack<BFSNode> stack, BFSNode node)
+        {
+            if (node.Parent != null)
+            {
+                stack.Push(node);
+                GetRootHelper(stack, node.Parent);
+            }
+        }
+    }
+}
